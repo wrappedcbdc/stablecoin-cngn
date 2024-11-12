65ebca5d2a1b4a7ffc78c6688155d7b6e9b83ca6
Zain <danbabagroup96@gmail.com>
2024-11-12 12:57:15 +0100

    commit

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,41 +3,45 @@ const { ethers, upgrades, run } = require("hardhat");
 async function main() {
    const gas = await ethers.provider.getGasPrice()
 
+   const overide = {
+    // gasPrice: gas
+   }
+
    // deplopy operations contract
-   // const adminContract = await ethers.getContractFactory("Admin");
-   // console.log("Deploying admin contract...");
-   //  const admin = await upgrades.deployProxy(adminContract, []
-   //    ,{
-   //    initializer: "initialize",
-   //    kind: "transparent",
-   // }
-   // )
-   // await admin.deployed();
-   // console.log("Upgradeable admin Contract deployed to:", admin.address);
-   
+   const adminContract = await ethers.getContractFactory("Admin");
+   console.log("Deploying admin contract...");
+    const admin = await upgrades.deployProxy(adminContract, []
+      ,{
+      initializer: "initialize",
+      kind: "transparent",
+   },
+   overide
+   )
+   await admin.deployed();
+   console.log("Upgradeable admin Contract deployed to:", admin.address);
 
    // deploy forwarder contract
    console.log("Deploying forwarder contract");
    const forwarderContract = await ethers.getContractFactory("MinimalForwarder");
-   const forwarder = await forwarderContract.deploy("0x38bF0493a0623aC592EB652BBDF4A18c8e1C52E5");
+   const forwarder = await forwarderContract.deploy(admin.address, overide);
    console.log("forwarder contract deployed to: ", forwarder.address);
 
-
    // deploy cngn contract
-   // const cngnContract = await ethers.getContractFactory("cngn");
-   // console.log("Deploying cngn contract...");
-   //  const cngn = await upgrades.deployProxy(cngnContract, [
-   //    forwarder.address,
-   //    admin.address
-   //  ]
-   //    ,{
-   //    initializer: "initialize",
-   //    kind: "transparent",
-   //    unsafeAllow: ['delegatecall']
-   // }
-   // )
-   // await cngn.deployed();
-   // console.log("Upgradeable cngn Contract deployed to:", cngn.address);
+   const cngnContract = await ethers.getContractFactory("cngn");
+   console.log("Deploying cngn contract...");
+    const cngn = await upgrades.deployProxy(cngnContract, [
+      forwarder.address,
+      admin.address
+    ]
+      ,{
+      initializer: "initialize",
+      kind: "transparent",
+      unsafeAllow: ['delegatecall']
+   },
+   overide
+   )
+   await cngn.deployed();
+   console.log("Upgradeable cngn Contract deployed to:", cngn.address);
 
 }
 
@@ -45,4 +49,4 @@ main().catch((error) => {
    console.error(error);
    process.exitCode = 1;
  });
-
+ 
