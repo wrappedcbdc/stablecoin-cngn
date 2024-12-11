800c31ac7b8d80f3a4d7abd2452c9e1f886e9cd9
justcharlz <charlie4biz@gmail.com>
2024-12-11 13:05:46 +0100

    fix: upgraded to EIP712 forwarder

--- contracts/forwarder.sol
+++ contracts/forwarder.sol
@@ -15,32 +15,67 @@ import "./IOperations.sol";
  * @dev Simple forwarder for extensible meta-transaction forwarding.
  * This contract has been updated with security improvements.
  */
-contract MinimalForwarder is EIP712, Ownable, Pausable, ReentrancyGuard {
+contract Forwarder is EIP712, Ownable, Pausable, ReentrancyGuard {
     using ECDSA for bytes32;
 
-    // Declare events
     event BridgeAuthorized(address indexed bridgeAddress);
     event BridgeDeauthorized(address indexed bridgeAddress);
     event AdminOperationsAddressUpdated(address indexed newAdminAddress);
-
+    event executed(bool indexed success, bytes indexed returnData);
+    
     struct ForwardRequest {
         address from;
         address to;
         uint256 value;
+        uint256 gas;
         uint256 nonce;
         bytes data;
     }
 
     address public adminOperationsContract;
+    uint256 private immutable _CHAIN_ID;
+    bytes32 private immutable _DOMAIN_SEPARATOR;
+    bytes32 public constant _TYPEHASH =
+        keccak256(
+            "ForwardRequest(address from,address to,uint256 value,uint256 gas,uint256 nonce,bytes data)"
+        );
+
     mapping(address => bool) public authorizedBridges; // Track authorized bridge contracts
     mapping(bytes32 => bool) public processedTxHashes; // Prevent replay attacks
-
     mapping(address => uint256) private _nonces;
 
+    modifier onlyAuthorizedBridge() {
+        require(authorizedBridges[msg.sender], "Unauthorized bridge");
+        _;
+    }
     constructor(
         address _adminOperationsContract
-    ) EIP712("MinimalForwarder", "0.0.1") {
+    ) EIP712("cNGN", "0.0.1") {
+        _CHAIN_ID = block.chainid;
+        _DOMAIN_SEPARATOR = _calculateDomainSeparator();
         adminOperationsContract = _adminOperationsContract;
+        authorizedBridges[msg.sender] = true;
+
+    }
+
+    function _calculateDomainSeparator() internal view returns (bytes32) {
+        return keccak256(
+            abi.encode(
+                keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
+                keccak256(bytes("CHATS")),
+                keccak256(bytes("0.0.1")),
+                block.chainid,
+                address(this)
+            )
+        );
+    }
+
+    function getDomainSeparator() public view returns (bytes32) {
+        return _DOMAIN_SEPARATOR;
+    }
+
+    function getChainId() public view returns (uint256) {
+        return _CHAIN_ID;
     }
 
     function updateAdminOperationsAddress(
@@ -55,24 +90,46 @@ contract MinimalForwarder is EIP712, Ownable, Pausable, ReentrancyGuard {
         return _nonces[from];
     }
 
-    function verify(
-        ForwardRequest calldata req,
-        bytes calldata signature
-    ) public view returns (bool) {
-        bytes32 messagehash = keccak256(
-            abi.encodePacked(req.from, req.to, req.value, req.nonce, req.data)
-        );
-        address signer = messagehash.toEthSignedMessageHash().recover(
-            signature
-        );
-
-        return ((_nonces[req.from] == req.nonce && signer == req.from));
+    function _preventReplay(bytes32 txHash) internal {
+        require(!processedTxHashes[txHash], "Replay attack prevented");
+        processedTxHashes[txHash] = true;
     }
 
-    function _executeTransaction(
+    function verify(ForwardRequest calldata req, bytes calldata signature)
+        public
+        view
+        returns (bool)
+    {
+        address signer = _hashTypedDataV4(
+            keccak256(
+                abi.encode(
+                    _TYPEHASH,
+                    req.from,
+                    req.to,
+                    req.value,
+                    req.gas,
+                    req.nonce,
+                    keccak256(req.data)
+                )
+            )
+        ).recover(signature);
+        return signer == req.from;
+    }
+
+    function authorizeBridge(address bridgeAddress) external onlyOwner {
+        authorizedBridges[bridgeAddress] = true;
+        emit BridgeAuthorized(bridgeAddress);  // Emit event
+    }
+
+    function deauthorizeBridge(address bridgeAddress) external onlyOwner {
+        authorizedBridges[bridgeAddress] = false;
+        emit BridgeDeauthorized(bridgeAddress);  // Emit event
+    }
+
+     function _executeTransaction(
         ForwardRequest calldata req,
         bytes calldata signature
-    ) private returns (bool, bytes memory) {
+    ) internal returns (bool, bytes memory) {
         require(
             IAdmin(adminOperationsContract).canForward(req.from),
             "You are not allowed to use this tx route"
@@ -89,9 +146,9 @@ contract MinimalForwarder is EIP712, Ownable, Pausable, ReentrancyGuard {
             IAdmin(adminOperationsContract).canMint(req.from),
             "Minter not authorized to sign"
         );
-        require(
+          require(
             verify(req, signature),
-            "MinimalForwarder: signature does not match request"
+            "Forwarder: signature does not match request"
         );
 
         bytes32 txHash = keccak256(
@@ -104,13 +161,20 @@ contract MinimalForwarder is EIP712, Ownable, Pausable, ReentrancyGuard {
             abi.encodePacked(req.data, req.from)
         );
 
+        if (success == true) {
+            emit executed(success, "execution is a success");
+        }
+        else {
+            emit executed(success, returndata);
+        }
+
         return (success, returndata);
     }
 
     function execute(
         ForwardRequest calldata req,
         bytes calldata signature
-    ) public payable onlyOwner whenNotPaused nonReentrant returns (bool, bytes memory) {
+    ) public payable onlyOwner  whenNotPaused nonReentrant  returns (bool, bytes memory) {
         return _executeTransaction(req, signature);
     }
 
@@ -134,26 +198,5 @@ contract MinimalForwarder is EIP712, Ownable, Pausable, ReentrancyGuard {
     function unpause() external onlyOwner {
         _unpause();
     }
-
-    function authorizeBridge(address bridgeAddress) external onlyOwner {
-        authorizedBridges[bridgeAddress] = true;
-        emit BridgeAuthorized(bridgeAddress);  // Emit event
-    }
-
-    function deauthorizeBridge(address bridgeAddress) external onlyOwner {
-        authorizedBridges[bridgeAddress] = false;
-        emit BridgeDeauthorized(bridgeAddress);  // Emit event
-    }
-
-    modifier onlyAuthorizedBridge() {
-        require(authorizedBridges[msg.sender], "Unauthorized bridge");
-        _;
-    }
-
-    function _preventReplay(bytes32 txHash) internal {
-        require(!processedTxHashes[txHash], "Replay attack prevented");
-        processedTxHashes[txHash] = true;
-    }
-
     receive() external payable {}
 }
