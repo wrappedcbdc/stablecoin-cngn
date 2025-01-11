26d7423ba218fdf3996d45d3de03a94a077f0655
justcharlz <charlie4biz@gmail.com>
2025-01-11 06:44:11 +0100

    fix: new contract deployed for mainnet

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,23 +4,29 @@ async function main() {
   // Deploy the Admin contract
   const Admin = await ethers.getContractFactory("Admin");
   console.log("Deploying Admin contract...");
-  const admin = await upgrades.deployProxy(Admin);
+  const admin = await upgrades.deployProxy(Admin,
+    {
+      initializer: "initialize",
+      kind: "transparent",
+      // unsafeAllow: ["delegatecall"],
+    }
+  );
   await admin.deployed();
   console.log("Admin contract deployed to:", admin.address);
 
 
-  // Automatically verify the proxy implementation
-  const implementationAddress = await upgrades.erc1967.getImplementationAddress(admin.address);
-  console.log("Implementation address:", implementationAddress);
+  // Automatically verify the Admin proxy implementation
+  const adminImplementationAddress = await upgrades.erc1967.getImplementationAddress(admin.address);
+  console.log("Admin Implementation address:", adminImplementationAddress);
 
   await hre.run("verify:verify", {
-    address: implementationAddress,
+    address: adminImplementationAddress,
   });
 
   // Deploy the Forwarder contract with the adminContract address
   const Forwarder = await ethers.getContractFactory("Forwarder");
   console.log("Deploying Forwarder contract...");
-  const forwarder = await Forwarder.deploy(admin);
+  const forwarder = await Forwarder.deploy(admin.address);
   await forwarder.deployed();
   console.log(
     "Forwarder contract deployed to:",
@@ -29,6 +35,7 @@ async function main() {
 
   await hre.run("verify:verify", {
       address: forwarder.address,
+      constructorArguments: [admin.address]
     });
 
   // Deploy the cngn contract via proxy
@@ -36,7 +43,7 @@ async function main() {
   console.log("Deploying cngn contract...");
   const cngn = await upgrades.deployProxy(
     cngnContract,
-    [forwarder.address, admin],
+    [forwarder.address, admin.address],
     {
       initializer: "initialize",
       kind: "transparent",
@@ -46,6 +53,14 @@ async function main() {
 
   await cngn.deployed();
   console.log("Upgradeable cngn Contract deployed to:", cngn.address);
+
+    // Automatically verify the Admin proxy implementation
+    const cngnImplementationAddress = await upgrades.erc1967.getImplementationAddress(admin.address);
+    console.log("cNGN Implementation address:", cngnImplementationAddress);
+  await hre.run("verify:verify", {
+      address: cngnImplementationAddress,
+      constructorArguments: [forwarder.address,admin.address], 
+    });
 }
 
 main()
