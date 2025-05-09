b5ffb7e69146dd33f6a1eae5ba7b9e9ff69da958
justcharlz <charlie4biz@gmail.com>
2025-05-09 18:40:16 +0100

    fix: removed pk

--- tron-contract/tronbox-config.js
+++ tron-contract/tronbox-config.js
@@ -21,7 +21,7 @@ module.exports = {
     // },
     development: {
       // privateKey: process.env.TRON_PRIVATE_KEY_LOCALNET,
-      privateKey: 'da146374a75310b9666e834ee4ad0866d6f4035967bfc76217c5a495fff9f0d0',
+      privateKey: '',
       consume_user_resource_percent: 30,
       fee_limit: 100000000,
       fullNode: "http://127.0.0.1:8090",
